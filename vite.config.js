b940d12fc4d47ab49f6b129ed5d55f2607f3cc0e
vrushabhh97 <vdeogirikar97@gmail.com>
2025-05-03 15:59:01 -0700

    vite config

--- vite.config.js
+++ vite.config.js
@@ -14,6 +14,6 @@ export default defineConfig({
     }
   },
   build: {
-    outDir: '../dist', // optional: for separating build output
+    outDir: 'dist', // optional: for separating build output
   }
 })
